adbdaf8c39d30d92d97f715a8e2d9f28e75e45fb
Tamanna Yasmin Shimu <tamannayasmin5050@gmail.com>
2025-11-22 15:28:42 +0600

    Create User.java

---
+++ GUI/User.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+public abstract class User implements Serializable {
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public abstract String getRole();
+}
